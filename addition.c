780c69128db11b368440962e5968b92e5bfb8907
khushi <160456172+kukki23@users.noreply.github.com>
2024-02-22 11:28:26 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    int a,b,sum=0;
+    printf("enter the two values two getting addition");
+    scanf("%d%d", &a,&b);
+    sum=a+b;
+    printf("sum of addition is %d",sum);
+}

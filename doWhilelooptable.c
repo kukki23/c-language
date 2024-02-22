5ca90f58dcecdf47dcfcdd3b53d71898bef159ca
khushi <160456172+kukki23@users.noreply.github.com>
2024-02-22 11:30:36 +0530

    Add files via upload

---
+++ doWhilelooptable.c
@@ -0,0 +1,27 @@
+#include <sdio.h>
+int main(){
+int n;
+printf("enter the table ");
+scanf("%d"&n);
+i=1;
+do{
+    printf
+    ("%d*%d=%d/n",n,i,i*n);
+    
+}
+  while(i<=10);  
+}#include <stdio.h>
+int main(){
+int n;
+int i;
+printf("enter the table ");
+scanf("%d",&n);
+i=1;
+do{
+    printf
+    (" %d * %d = %d\n",n,i,n*i);
+    i++;
+}
+  while(0); 
+  return 0;
+}

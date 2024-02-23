e8d02a025832128a482177eb019571eb14a6d080
khushi <160456172+kukki23@users.noreply.github.com>
2024-02-23 10:52:40 +0530

    Add files via upload

---
+++ real_leap_year.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int year;
+    printf("enter the leap year");
+    scanf("%d",&year);
+
+    if(( year%4==0)&&( year%100!=0)){
+        printf("%d year is leap year",year);
+    }
+    else         {
+        printf("%d is not leap year",year);
+    }
+}

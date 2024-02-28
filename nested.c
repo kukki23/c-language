b125d20db81807c29eb39f8d1bcd836b710482e3
khushi <160456172+kukki23@users.noreply.github.com>
2024-02-28 12:37:50 +0530

    Add files via upload

---
+++ nested.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) { 
+  int num=0;
+  int i;
+  int count=0;
+  for(i=1;i<=10;i++){
+    for(int j=1;j<=5;j++){
+      num+=1;
+    count+=1;
+  
+  printf("%d Hello World\n",count);}
+  }
+  return 0;
+}
---
+++ nestedloop.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+  int input;
+  scanf("%d",&input);
+  for(int i=1;i<=input;i++){
+    for(int j=1;j<=i;j++){
+    
+    printf("%d ", j);
+  }
+    printf("\n");
+}
+}
